d57f7d1d8e3eebebef4dc3a2e4678ca5daa7f0de
pablo <padilo@gmail.com>
2013-09-28 15:43:10 +0200

    first 6 problems

---
+++ src/p01/p01.scala
@@ -0,0 +1,16 @@
+package p01
+
+import java.util.NoSuchElementException
+
+object p01 {
+  def main(args: Array[String]):Unit = {
+	 println(last(List(1, 1, 2, 3, 5, 8)))
+	 println(last(List()))
+  }
+  
+  def last(l:List[Int]):Int = l match {
+    case head::List() => head
+    case head::tail => last(tail)
+    case _ => throw new NoSuchElementException
+  }
+}
---
+++ src/p03/p03.scala
@@ -0,0 +1,23 @@
+package p03
+
+import java.util.NoSuchElementException
+
+object p03 {
+	def main(args: Array[String]):Unit = {
+		println(nth(23, List(1, 1, 2, 3, 5, 8)))
+		println(nthMatch(23, List(1, 1, 2, 3, 5, 8)))
+	}
+	
+	def nth(i:Int, l:List[_]) = {
+		l(i)
+	}
+
+	def nthMatch(i:Int, l:List[_]):Any = {
+		l match {
+		  case head::_ if(i==0) => head
+		  case _::tail => nthMatch(i-1, tail)
+		  case _ => throw new NoSuchElementException
+		}
+	}
+
+}
