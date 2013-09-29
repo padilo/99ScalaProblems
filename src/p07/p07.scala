ad5267c445a27fdd72bfde956bcd536d3c6a88fc
pablo <padilo@gmail.com>
2013-09-29 11:45:03 +0200

    better solution

--- src/p07/p07.scala
+++ src/p07/p07.scala
@@ -2,17 +2,24 @@ package p07
 
 object p07 {
   def main(args: Array[String]): Unit = {
-    println(flatten(List(List(1, 1), 2, List(3, List(5, 8)))))
+    println(flatten2(List(List(1, 1), 2, List(3, List(5, 8)))))
   }
 
   def flatten(l: List[Any]): List[Any] = {
-    def flattenTR(l:Any, result:List[Any]): List[Any] = {
-	    l match {
+    def flattenTR(elem:Any, result:List[Any]): List[Any] = {
+	    elem match {
 	      case List() => result
 	      case x::tail => flattenTR(x, result):::flattenTR(tail, result):::result
 	      case x => x::result
 	    }
     }
     flattenTR(l, List())
+  }
+
+  def flatten2(l: List[Any]): List[Any] = {
+    l flatMap {
+      case x:List[_] => flatten2(x)
+      case e => List(e)
+    }
   }
 }
